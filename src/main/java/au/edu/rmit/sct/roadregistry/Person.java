7d998dceef593adb612c8e3e65a47c1e1322855a
NIRISHKA BALLAH <s4082045@student.rmit.edu.au>
2025-06-02 14:40:03 +1000

    Added default constructor to Person class

--- src/main/java/au/edu/rmit/sct/roadregistry/Person.java
+++ src/main/java/au/edu/rmit/sct/roadregistry/Person.java
@@ -10,8 +10,15 @@ public class Person {
     private String lastName;
     private String address;
     private String birthdate;
+    private int age;
     private HashMap<Date, Integer> demeritPoints; // offenseDate -> demerit points
     private boolean isSuspended;
+
+    public Person(){
+        this.demeritPoints = new HashMap<>();
+        this.isSuspended = fasle;
+        this.age = 25;
+    }
 
     public boolean addPerson() {
         // Indicate that are implementing this here:
