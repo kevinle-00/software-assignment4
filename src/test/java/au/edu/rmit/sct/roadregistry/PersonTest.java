1ed9148d60e405362e7929762e08f24bc359560d
jackb924 <s4007760@student.rmit.edu.au>
2025-06-05 15:36:45 +1000

    file cleanup added for persons txt

--- src/test/java/au/edu/rmit/sct/roadregistry/PersonTest.java
+++ src/test/java/au/edu/rmit/sct/roadregistry/PersonTest.java
@@ -1,5 +1,8 @@
 package au.edu.rmit.sct.roadregistry;
 
+import java.io.File;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +11,14 @@ public class PersonTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
+    }
+
+    @AfterEach
+    public void cleanUpPersonFile(){
+        File file = new File("persons.txt");
+        if(file.exists()) {
+            boolean deleted = file.delete();
+            System.out.println(deleted ? "Cleanup: persons.txt deleted." : "Cleanup: Failed to delete persons.txt.");
+        }
     }
 }
