bdd2b9a4d8252650ca3c5ef506ef1a8531598ad1
jackb924 <s4007760@student.rmit.edu.au>
2025-06-05 15:19:40 +1000

    addPerson test in main file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,17 @@
+
+import au.edu.rmit.sct.roadregistry.Person;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Person person = new Person(
+                "56s_d%&fAB",
+                "John",
+                "Doe",
+                "32|Highland Street|Melbourne|Victoria|Australia",
+                "15-11-1990");
+
+        person.addPerson();
+
     }
 }
